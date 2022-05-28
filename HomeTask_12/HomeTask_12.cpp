780daa24f08eeaf8e4b0178a0f4b6e87a372082a
Jimmy Blackwood <Jimmy Blackwood@DESKTOP-AOM0ABL>
2022-05-28 14:28:38 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_12/HomeTask_12.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int num, n1, n2, n3, n4, temp;
+    cout << "Введите четырёхзначное число: \n";
+    cin >> num;
+    if (num / 1000 < 1||num / 1000 > 9){
+        cout << " Вы ввели не четырёхзначное число! \n";
+    }
+    else {
+        cout << " Считаю! \n";
+        n4 = num % 10;
+        temp = num / 10;
+        n3 = temp % 10;
+        temp = temp / 10;
+        n2 = temp % 10;
+        temp = temp / 10;
+        n1 = temp % 10;
+        temp = temp / 10;
+        cout << "Было: " << endl;
+        cout << n1 << " " << n2 << " " << n3 << " " << n4 << endl;
+        cout << "Стало: " << endl;
+        cout << n2 << " " << n1 << " " << n4 << " " << n3 << endl;
+    }
+}
+
+
